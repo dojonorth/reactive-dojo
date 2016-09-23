9329d4405147ac425e05dab91467d875feb97fd3
Simon Scarfe <simon.scarfe@gmail.com>
2016-09-23 17:38:33 +0100

    hook in a templating library

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 app.get('/', function (req, res) {
-  res.send('Welcome!');
+  res.render('index.html');
 });
+
+if (!require("piping")()) { return; }
 
 app.listen(3000, function () {
   console.log('LET THE DOJO BEGIN! (go to http://localhost:3000/ )');
