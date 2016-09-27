3090be5b12e106dfa93f52cfff000ef6640e3f07
Simon Scarfe <simon.scarfe@gmail.com>
2016-09-27 13:49:38 +0100

    initial template

--- test/arrayTest.js
+++
@@ -1,9 +0,0 @@
-var assert = chai.assert;
-
-describe('Array', function() {
-  it('should return an array of size 0', function() {
-    var arr = getArray();
-
-    assert.equal(arr.length, 0);
-  });
-});
---
+++ test/fundamentalsTest.js
@@ -0,0 +1,17 @@
+var assert = chai.assert;
+
+describe('Fundamentals / native', function() {
+  describe('The bare minimum', function() {
+    it('map'); // square
+    it('filter'); // odds
+    it('reduce'); // sum
+    it('combine the above'); // find me the total of the odd squares
+    it('foreach'); // side effects / update value?
+  });
+
+  describe('Observables and further concepts', function() {
+    it('observables feature all of the above')
+    it('subscribe is an alias for foreach');
+    it('more methods - zip, concatAll etc');
+  });
+});
